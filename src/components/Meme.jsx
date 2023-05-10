dc3c704286ac5554dba11b80493c0bf4486f343b
wandakalinowska <wanda0501@gmail.com>
2023-05-10 19:36:07 +0200

    Added Meme component

---
+++ src/components/Meme.jsx
@@ -0,0 +1,37 @@
+
+const Meme = ({ id, item, handleUpvotes, handleDownvotes }) => {
+  let sum = -item.downvotes + item.upvotes;
+  const alt = "Tu miał być obrazek";
+
+  return (
+    <div key={id} className="meme">
+      <h2 className="meme-title">{item.title}:</h2>
+      <div className="meme-img">
+        <img src={item.img} alt={alt} />
+      </div>
+      <div className="meme-container">
+        <div className="meme-vote">
+          <p>{item.upvotes}</p>
+          <button
+            className="btn meme-vote__btn-up"
+            onClick={() => handleUpvotes(id)}
+          >
+            +
+          </button>
+        </div>
+        <div className="meme-vote">
+          <p>-{item.downvotes}</p>
+          <button
+            className="btn meme-vote__btn-down"
+            onClick={() => handleDownvotes(id)}
+          >
+            -
+          </button>
+        </div>
+      </div>
+      <div className="meme-vote__sum">Suma głosów: {sum}</div>
+    </div>
+  );
+};
+
+export default Meme;
