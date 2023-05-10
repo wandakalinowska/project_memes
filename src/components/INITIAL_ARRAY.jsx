fc488fee2a7323a256b675e70901695a050df658
wandakalinowska <wanda0501@gmail.com>
2023-05-10 17:11:14 +0200

    added components

---
+++ src/components/INITIAL_ARRAY.jsx
@@ -0,0 +1,61 @@
+const INITIAL_ARRAY = [
+    {
+      id: "e1",
+      title: "Mem 1",
+      upvotes: 2,
+      downvotes: 15,
+      img: "https://i1.kwejk.pl/k/obrazki/2023/04/9N2IRmVCqQgxtGbO.jpg"
+    },
+    {
+      id: "e2",
+      title: "Mem 2",
+      upvotes: 15,
+      downvotes: 26,
+      img: "https://i1.kwejk.pl/k/obrazki/2023/04/Yy1C1ogLa0lJTjcT.jpg"
+    },
+    {
+      id: "e3",
+      title: "Mem 3",
+      upvotes: 15,
+      downvotes: 4,
+      img: "https://i1.kwejk.pl/k/obrazki/2023/04/yTO6S7x3af0VgZXU.jpg"
+    },
+    {
+      id: "e4",
+      title: "Mem 4",
+      upvotes: 2,
+      downvotes: 16,
+      img: "https://i1.kwejk.pl/k/obrazki/2023/04/kLERK7sETJMdVNpn.jpg"
+    },
+    {
+      id: "e5",
+      title: "Mem 5",
+      upvotes: 1,
+      downvotes: 8,
+      img: "https://i1.kwejk.pl/k/obrazki/2023/04/dhVQFTls6OZgZGwK.jpg"
+    },
+    {
+      id: "e6",
+      title: "Mem 6",
+      upvotes: 9,
+      downvotes: 1,
+      img: "https://i1.kwejk.pl/k/obrazki/2023/04/mEsVUdDGQc8FWQdx.jpg"
+    },
+    {
+      id: "e7",
+      titlet: "Mem 7",
+      upvotes: 15,
+      downvotes: 6,
+      img: "https://i1.kwejk.pl/k/obrazki/2023/04/P0FheDXm6v9O3A8Z.jpg"
+    },
+    {
+      id: "e8",
+      title: "Mem 8",
+      upvotes: 15,
+      downvotes: 9,
+      img: "https://i1.kwejk.pl/k/obrazki/2023/04/c23iKh56GfgmyKIO.jpg"
+    }
+  ];
+  
+  export default INITIAL_ARRAY;
+  
